6b27999a8067f460af16734ea32126b54b7b5c39
gpdax <66251031+gpdax@users.noreply.github.com>
2023-09-02 00:54:27 -0300

    Update about.php

--- about.php
+++ about.php
@@ -13,24 +13,24 @@
                     <!-- Intro Detail -->
                     <div class="col-md-7">
                         <div class="text-sec padding-right-0">
-                            <h5>Check Up de saúde no Hospital Management System</h5>
-                            <p>Bem-vindo ao HMS, onde a cura e a compaixão se unem para fornecer cuidados médicos excepcionais. Estabelecido com a visão de servir a nossa comunidade e muito mais, o nosso hospital é um destino de cuidados de saúde confiável há 10 anos. Temos imenso orgulho de estar na vanguarda da inovação médica, sempre nos esforçando para oferecer o mais alto padrão de serviços centrados no paciente.</p>
+                            <h5>Health Check Up At Hospital Management System</h5>
+                            <p>Welcome to HMS, where healing and compassion unite to provide exceptional medical care. Established with a vision to serve our community and beyond, our hospital has been a trusted healthcare destination for 10 years. We take immense pride in being at the forefront of medical innovation, always striving to deliver the highest standard of patient-centered services.</p>
                             <ul class="row">
                               <li class="col-sm-6">
-                                <h6> <i class="lnr  lnr-checkmark-circle"></i> Casos de Emergência</h6>
-                                <p>O HMS fornece comunicações confiáveis e melhora as áreas de segurança pública. </p>
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> EMERGENCY CASE</h6>
+                                <p>HMS provide reliable communications and improve public safety areas. </p>
                               </li>
                               <li class="col-sm-6">
-                                <h6> <i class="lnr  lnr-checkmark-circle"></i> Médicos Qualificados</h6>
-                                <p>Hopital Management System tem médicos licenciados e qualificados! </p>
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> QUALIFIED DOCTORS</h6>
+                                <p>Hopital Management System has licensed and qualified doctors! </p>
                               </li>
                               <li class="col-sm-6">
-                                <h6> <i class="lnr  lnr-checkmark-circle"></i> Consultas Online</h6>
-                                <p>Hospital Management System possui consultas online. Verifique a guia de agendamento acima. </p>
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> ONLINE APPOINTMENT</h6>
+                                <p>Hospital Management System Supports Online Appointment. Check the appointment tab above. </p>
                               </li>
                               <li class="col-sm-6">
-                                <h6> <i class="lnr  lnr-checkmark-circle"></i> Aconselhamento médico grautuito</h6>
-                                <p>HMS possui médicos qualificados que oferecem aconselhamento gratuito aos seus pacientes. </p>
+                                <h6> <i class="lnr  lnr-checkmark-circle"></i> FREE MEDICAL COUNSELING</h6>
+                                <p>HMS qualified doctors offers free counseling to their patients. </p>
                               </li>
                             </ul>
                         </div>
